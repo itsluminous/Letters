cb4ee873ab045b7d69ad01e47dc14074ab3111cb
Prakash <kupraki@amazon.com>
2025-11-23 09:35:53 +0530

    Revert "Add manifest for PWA"
    This reverts commit 5d439116083615dcc275085abd2039a80c58daa2.

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,6 @@ const caveat = Caveat({
 export const metadata: Metadata = {
   title: "Letters",
   description: "Exchange letters through time.",
-  manifest: "/manifest.json",
   icons: {
     icon: [
       {
@@ -38,19 +37,6 @@ export const metadata: Metadata = {
         type: "image/svg+xml",
       },
     ],
-    apple: [
-      {
-        url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' fill='%235c4a2f'><path d='M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z'/><path d='M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z'/></svg>",
-        sizes: "180x180",
-        type: "image/svg+xml",
-      },
-    ],
-  },
-  themeColor: "#d4c4a8",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
   },
 };
 
